116d0808ad4521372532d58a3b72555be878cc63
williamdes <williamdes@wdes.fr>
2015-06-06 22:09:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ header('Content-Type: application/json');
 $jsonData = Array(
 	"Status" => "OK",
 	"Runtime-Mode" => "productionMode",
-	"Application-Author" => "NathaanGaming",
+	"Application-Author" => "NathaanGaming_et_Wdes",
 	"Application-Description" => "Remake of Mojang Authentication Server (Yggdrasil) for LaunchMyCraft.",
 	"Specification-Version" => "0.2",
 	"Application-Name" => "launchmycraft.auth.server",
 	"Implementation-Version" => "0.2_beta",
-	"Application-Owner" => "NathaanGaming"
+	"Application-Owner" => "NathaanGaming_et_Wdes"
 );
 
 echo json_encode($jsonData);
