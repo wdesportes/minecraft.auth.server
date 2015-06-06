313995c33129961cb6ef20f733979b57360e86a6
williamdes <williamdes@wdes.fr>
2015-06-06 21:42:38 +0200

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Installation automatisée : 1ère étape</title>
+<meta charset="UTF-8">
+<title>Installation automatisÃ©e : 1Ã¨re Ã©tape</title>
 <style type="text/css">
 body {
 font-family:Tahoma, Arial, Serif; /* polices du texte */
@@ -16,22 +16,22 @@ label
 font-size:1.2em; /* taille du texte pour les "label" */
 display:block; /* on affiche les "label" en tant que block et non pas inline */ 
 width:150px; /* on leur met une taille pour aligner nos zones de texte */
-float:left; /* flottant à gauche */
+float:left; /* flottant Ã  gauche */
 }
 </style>
 </head>
 <body>
 
-<h1>Informations sur la base de données MySQL :</h1>
+<h1>Informations sur la base de donnÃ©es MySQL :</h1>
 <p>
 <form action="traitement.php" method="post">
 <p>
 <input type="hidden" name="etape" value="1" />
 
-<label for="hote">Hôte :</label>
+<label for="hote">HÃ´te :</label>
 <input type="text" name="hote" maxlength="40" /><br />
 
-<label for="port">Port (défaut 3306) :</label>
+<label for="port">Port (dÃ©faut 3306) :</label>
 <input tpye="text" name="port" maxlenght="40" /><br />
 
 <label for="login">Utilisateur :</label>
@@ -43,7 +43,7 @@ float:left; /* flottant à gauche */
 <label for="base">Nom de la base :</label>
 <input type="text" name="base" maxlength="40" /><br /><br />
 
-<label for="key">Clé privée :</label>
+<label for="key">ClÃ© privÃ©e :</label>
 <input type="text" name="key" maxlength="40" /><br /><br />
 
 <label for="submit">&nbsp;</label>
