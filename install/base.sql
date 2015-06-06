9799c71a58953b0a78b932146d46b5a25c3395fb
williamdes <williamdes@wdes.fr>
2015-06-06 21:54:24 +0200

    Update base.sql

--- install/base.sql
+++ install/base.sql
@@ -3,14 +3,14 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 12, 2015 at 12:55 PM
+-- Generation Time: Juin 06, 2015 at 21:54 PM
 -- Server version: 5.1.57
 -- PHP Version: 5.2.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Crée par NathaanGaming
+-- CrÃ©e par NathaanGaming et mis a jour pour pdo par Wdes
 --
 
 -- --------------------------------------------------------
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `tokens` (
   `clientToken` char(255) COLLATE latin1_general_ci NOT NULL,
   `uuid` char(32) COLLATE latin1_general_ci NOT NULL,
   UNIQUE KEY `accessToken` (`accessToken`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `tokens`
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` char(16) COLLATE latin1_general_ci NOT NULL,
   `password` char(32) COLLATE latin1_general_ci NOT NULL,
   UNIQUE KEY `uuid` (`uuid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `users`
